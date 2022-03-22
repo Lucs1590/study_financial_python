3b939a6bc9ca24483e3bc7506e2206ac0a3712c5
Lucas Brito <lucasbsilva29@gmail.com>
2022-03-22 19:16:04 -0300

    feat(rate.py): translate and applying pylint

--- scripts/rate.py
+++ scripts/rate.py
@@ -4,49 +4,48 @@ from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 
 
-def trs(acao):
-    acao['simple_return'] = (
-        acao['Adj Close'] / acao['Adj Close'].shift(1)) - 1
-    print('De {} à {}'.format(
-        data_BR(acao['simple_return'].index[0]), data_BR(acao['simple_return'].index[-1])))
-    acao['simple_return'].plot(figsize=(8, 5))
+def trs(_active):
+    _active['simple_return'] = (
+        _active['Adj Close'] / _active['Adj Close'].shift(1)) - 1
+    print(
+        f'From {to_br_date(_active["simple_return"].index[0])} to {to_br_date(_active["simple_return"].index[-1])}')
+    _active['simple_return'].plot(figsize=(8, 5))
     plt.show()
-    avg_return_d = acao['simple_return'].mean()
-    avg_return_a = acao['simple_return'].mean() * 250
+    avg_return_d = _active['simple_return'].mean()
+    avg_return_a = _active['simple_return'].mean() * 250
     return round(avg_return_a * 100, 3), round(avg_return_d * 100, 3)
 
 
-def trl(acao):
-    acao['log_return'] = np.log(
-        acao['Adj Close'] / acao['Adj Close'].shift(1))
-    print('De {} à {}'.format(
-        data_BR(acao['log_return'].index[0]), data_BR(acao['log_return'].index[-1])))
-    acao['log_return'].plot(figsize=(8, 5))
+def trl(_active):
+    _active['log_return'] = np.log(
+        _active['Adj Close'] / _active['Adj Close'].shift(1))
+    print(
+        f'From {to_br_date(_active["log_return"].index[0])} to {to_br_date(_active["log_return"].index[-1])}')
+    _active['log_return'].plot(figsize=(8, 5))
     plt.show()
-    log_return_d = acao['log_return'].mean()
-    log_return_a = acao['log_return'].mean() * 250
+    log_return_d = _active['log_return'].mean()
+    log_return_a = _active['log_return'].mean() * 250
     return round(log_return_a * 100, 3), round(log_return_d * 100, 3)
 
 
-def data_BR(data):
-    # 2018-05-11 00:00:00
+def to_br_date(data) -> str:
     data = str(data)
     return data[8:10] + '/' + data[5:7] + '/' + data[:4]
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-a", "--ativo", required=True, help="Sequence")
-ap.add_argument("-o", "--opcao", type=int, required=True,
-                help="Opções:\n 1-Taxa de retorno simples\n 2-Taxa de retorno logaritimaca")
+ap.add_argument("-a", "--ative", required=True, help="Sequence")
+ap.add_argument("-o", "--option", type=int, required=True,
+                help="Options:\n 1-Simple Return Rate\n 2-Logarithmic Return Rate")
 args = vars(ap.parse_args())
 
-acao = wb.DataReader(args['ativo'], data_source='yahoo', start='2020-3-11')
+active = wb.DataReader(args['ativo'], data_source='yahoo', start='2020-3-11')
 
-if (args["opcao"] == 1):
-    tr = trs(acao)
-    print(' > {}% ao ano\n > {}% ao dia'.format(tr[0], tr[1]))
-elif (args["opcao"] == 2):
-    tr = trl(acao)
-    print(' > {}% ao ano\n > {}% ao dia'.format(tr[0], tr[1]))
+if args["option"] == 1:
+    tr = trs(active)
+    print(f' > {tr[0]}% by year\n > {tr[1]}% by day')
+elif args["option"] == 2:
+    tr = trl(active)
+    print(f' > {tr[0]}% by year\n > {tr[1]}% by day')
 else:
-    print("Digite uma opcao valida!")
+    print("Try to digite a valid option, in this case, 1 or 2")
