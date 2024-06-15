ee843d23d324032df417623af2098498ff713835
Lucas Brito <lucasbsilva29@gmail.com>
2024-06-15 14:07:21 -0300

    style: indenting the code

--- scripts/portfolio_risk.py
+++ scripts/portfolio_risk.py
@@ -50,64 +50,70 @@ def calculate_diversifiable_risk(returns, weights):
     return diversifiable_risk
 
 
-# Define tickers and date range
-tickers = ['VGIR11.SA', 'BTCI11.SA', 'RURA11.SA', 'MXRF11.SA', 'GALG11.SA']
-start_date = '2023-06-01'
-end_date = datetime.now()
+def main():
+    # Define tickers and date range
+    tickers = ['VGIR11.SA', 'BTCI11.SA', 'RURA11.SA', 'MXRF11.SA', 'GALG11.SA']
+    start_date = '2023-06-01'
+    end_date = datetime.now()
 
-# Download data
-data = download_data(tickers, start_date, end_date)
+    # Download data
+    data = download_data(tickers, start_date, end_date)
 
-# Calculate returns
-returns = calculate_returns(data)
+    # Calculate returns
+    returns = calculate_returns(data)
 
-# Calculate statistics
-mean_returns, std_returns, mean_annual_returns, std_annual_returns = calculate_statistics(
-    returns
-)
+    # Calculate statistics
+    mean_returns, std_returns, mean_annual_returns, std_annual_returns = calculate_statistics(
+        returns
+    )
 
-# Print statistics
-for ticker in tickers:
-    print(f'{ticker} mean: {mean_returns[ticker] * 100}')
-    print(f'{ticker} mean annual: {mean_annual_returns[ticker] * 100}')
-    print(f'{ticker} std: {std_returns[ticker] * 100}')
-    print(f'{ticker} std annual: {std_annual_returns[ticker]}\n')
+    # Print statistics
+    for ticker in tickers:
+        print(f'{ticker} mean: {mean_returns[ticker] * 100}')
+        print(f'{ticker} mean annual: {mean_annual_returns[ticker] * 100}')
+        print(f'{ticker} std: {std_returns[ticker] * 100}')
+        print(f'{ticker} std annual: {std_annual_returns[ticker]}\n')
 
-# Calculate portfolio metrics
-# Equal weighting scheme dinamically
-weights = np.full(len(tickers), 1 / len(tickers))
-pfolio_var, pfolio_volatility = calculate_portfolio_metrics(returns, weights)
-print(f'Portfolio Variance: {pfolio_var * 100}')
-print(f'Portfolio Volatility: {pfolio_volatility * 100}')
+    # Calculate portfolio metrics
+    # Equal weighting scheme dinamically
+    weights = np.full(len(tickers), 1 / len(tickers))
+    pfolio_var, pfolio_volatility = calculate_portfolio_metrics(
+        returns, weights)
+    print(f'Portfolio Variance: {pfolio_var * 100}')
+    print(f'Portfolio Volatility: {pfolio_volatility * 100}')
 
-# Calculate diversifiable risk
-diversifiable_risk = calculate_diversifiable_risk(returns, weights)
-print(f'Diversifiable Risk: {diversifiable_risk * 100}')
-if diversifiable_risk > 0:
-    non_diversifiable_risk = pfolio_var - diversifiable_risk
-    print(f'Non-Diversifiable Risk: {non_diversifiable_risk * 100}')
+    # Calculate diversifiable risk
+    diversifiable_risk = calculate_diversifiable_risk(returns, weights)
+    print(f'Diversifiable Risk: {diversifiable_risk * 100}')
+    if diversifiable_risk > 0:
+        non_diversifiable_risk = pfolio_var - diversifiable_risk
+        print(f'Non-Diversifiable Risk: {non_diversifiable_risk * 100}')
 
-portfolio_return = np.dot(weights, mean_annual_returns)
-print(f'Portfolio Return: {portfolio_return * 100} (annual)')
-print(f'Portfolio Return: {portfolio_return * 100 / 12} (monthly)')
-print(
-    f'Portfolio Sharpe Ratio: {portfolio_return / pfolio_volatility}' if pfolio_volatility != 0 else 'Infinity'
-)
+    portfolio_return = np.dot(weights, mean_annual_returns)
+    print(f'Portfolio Return: {portfolio_return * 100} (annual)')
+    print(f'Portfolio Return: {portfolio_return * 100 / 12} (monthly)')
+    print(
+        f'Portfolio Sharpe Ratio: {portfolio_return / pfolio_volatility}' if pfolio_volatility != 0 else 'Infinity'
+    )
 
-print("""\n - If the diversifiable risk is greater than zero it means that the portfolio is not well diversified.
-   In this case, the non-diversifiable risk is also calculated, which is the risk that cannot be diversified away.""")
-print(""" - If the diversifiable risk is zero, it means that the portfolio is well diversified and the non-diversifiable risk is equal to the portfolio variance.""")
-print(""" - The variance of the portfolio represents the total risk of the portfolio, which is the sum of the diversifiable and non-diversifiable risk.""")
-print(""" - The volatility of the portfolio is the standard deviation of the portfolio returns, which is a measure of the total risk of the portfolio.""")
-print(""" - The Sharpe ratio is a measure of the risk-adjusted return of the portfolio, which is the portfolio return divided by the portfolio volatility.
-   If the sharpe ratio is greater, it means that the portfolio is generating more return for the risk taken. If it is lower, it means that the portfolio is generating less return for the risk taken.""")
+    print("""\n - If the diversifiable risk is greater than zero it means that the portfolio is not well diversified.
+    In this case, the non-diversifiable risk is also calculated, which is the risk that cannot be diversified away.""")
+    print(""" - If the diversifiable risk is zero, it means that the portfolio is well diversified and the non-diversifiable risk is equal to the portfolio variance.""")
+    print(""" - The variance of the portfolio represents the total risk of the portfolio, which is the sum of the diversifiable and non-diversifiable risk.""")
+    print(""" - The volatility of the portfolio is the standard deviation of the portfolio returns, which is a measure of the total risk of the portfolio.""")
+    print(""" - The Sharpe ratio is a measure of the risk-adjusted return of the portfolio, which is the portfolio return divided by the portfolio volatility.
+    If the sharpe ratio is greater, it means that the portfolio is generating more return for the risk taken. If it is lower, it means that the portfolio is generating less return for the risk taken.""")
 
-# Save data to a CSV file with the metrics
-metrics = pd.DataFrame({
-    'Mean': mean_returns,
-    'Mean Annual': mean_annual_returns,
-    'Std': std_returns,
-    'Std Annual': std_annual_returns
-})
-metrics.to_csv('metrics.csv')
-data.to_csv('data.csv')
+    # Save data to a CSV file with the metrics
+    metrics = pd.DataFrame({
+        'Mean': mean_returns,
+        'Mean Annual': mean_annual_returns,
+        'Std': std_returns,
+        'Std Annual': std_annual_returns
+    })
+    metrics.to_csv('metrics.csv')
+    data.to_csv('data.csv')
+
+
+if __name__ == '__main__':
+    main()
